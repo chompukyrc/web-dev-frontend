5280be0c22336d15f11f881c4f73b1eca237cc4c
mmayyiisuay <queenzio@MacMay.local>
2023-04-27 22:51:03 +0700

    responsive nav

--- src/layout/default.jsx
+++ src/layout/default.jsx
@@ -1,24 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import Dialog from '../components/dialog'
-import Avatar, { genConfig } from 'react-nice-avatar'
+// import Dialog from '../components/dialog'
+// import Avatar, { genConfig } from 'react-nice-avatar'
 import axios from 'axios'
+import NotificationIcon from '../components/notification/notificationIcon'
+import ProfileIcon from '../components/profile/profileIcon'
 
 const Layout = ({ children }) => {
     const navigate = useNavigate()
     const token = localStorage.getItem('token')
-    const [open, setOpen] = useState(false)
+    // const [open, setOpen] = useState(false)
     const [profile, setProfile] = useState({})
-
-    const handleConfirm = (result) => {
-        if (result) {
-            // console.log('some action...')
-            localStorage.clear()
-            window.location.reload()
-        }
-
-        setOpen(false)
-    }
 
     useEffect(() => {
         const fetchProfile = async () => {
@@ -38,91 +30,60 @@ const Layout = ({ children }) => {
         fetchProfile()
     }, [])
 
-    const config = genConfig(profile.firstname + profile.lastname)
+    // const config = genConfig(profile.firstname + profile.lastname)
 
     return (
         <div>
-            <div className="bg-[#60B664] h-[60px] md:h-auto flex w-[100%] justify-between items-center font-Kanit text-[20px]">
+            <div className="bg-[#60B664] flex w-screen justify-between font-Kanit text-[20px] py-1">
                 <div
-                    className="flex flex-row items-center text-white cursor-pointer md:ml-10% ml-[5%]"
+                    className="flex items-center text-white cursor-pointer ml-40"
                     onClick={() => {
                         navigate('/')
                     }}
                 >
-                    <p className="w-20 mr-[2%]">🍔 Feed</p>
-                    <p className="text-amber-300 mr-[2%]">My</p>
-                    <p className="mr-[2%]">Friend</p>
+                    🍔&nbsp;&nbsp; Feed
+                    <p className="text-amber-300">&nbsp; My &nbsp;</p> Friend
                 </div>
-                <div className="md:block hidden mr-[5%] w-1/2">
-                    <div className="flex items-center ">
-                        <div
-                            className="flex justify-center hover:bg-[#4da452] cursor-pointer text-white w-1/4 py-4 mx-[2%]"
-                            onClick={() => {
-                                navigate('/')
-                            }}
-                        >
-                            <i className="mdi mdi-home mr-1"></i>
-                            หน้าหลัก
-                        </div>
-
-                        <div
-                            className="flex justify-center hover:bg-[#4da452] cursor-pointer text-white  w-1/4 py-4 mx-[2%]"
-                            onClick={() => {
-                                navigate('/aboutUs')
-                            }}
-                        >
-                            <i className="mdi mdi-developer-board mr-1"></i>
-                            เกี่ยวกับเรา
-                        </div>
-                        <div
-                            className="flex justify-center hover:bg-[#4da452] md:flex cursor-pointer text-white  w-1/6 py-4 mx-[2%]"
-                            onClick={() => {
-                                navigate('/profile')
-                            }}
-                        >
-                            <Avatar className="mr-3 w-8 h-8" {...config} />
-                            <p className="hidden md:block">
-                                {profile.username}
-                            </p>
-                        </div>
-                        <div
-                            className="md:block hidden hover:bg-[#4da452] cursor-pointer text-white w-1/4 py-4 mx-[2%]"
-                            onClick={() => setOpen(true)}
-                        >
-                            <i className="mdi mdi-logout mr-1"></i>
-                            ออกจากระบบ
-                        </div>
-                        <Dialog
-                            text={'Are you sure to Logout?'}
-                            open={open}
-                            handleConfirm={handleConfirm}
-                            textConfirm={'LOGOUT'}
-                        />
+                <div className="flex items-center mr-20 space-x-1">
+                    <div
+                        className="hover:bg-[#4da452] cursor-pointer text-white flex justify-center items-center w-40 py-2"
+                        onClick={() => {
+                            navigate('/')
+                        }}
+                    >
+                        <i className="mdi mdi-home px-1"></i>
+                        หน้าหลัก
                     </div>
-                </div>
-                <div class="block md:hidden mr-[5%]">
-                    <button class="flex items-center  rounded text-white ">
-                        <svg
-                            
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="20"
-                            height="14"
-                            viewBox="0 0 20 14"
-                            id="menu"
-                        >
-                            <g
-                                fill="none"
-                                fill-rule="evenodd"
-                                stroke="#FFF"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                transform="translate(1 1)"
-                            >
-                                <path d="M0 6h18M0 0h18M0 12h18"></path>
-                            </g>
-                        </svg>
-                    </button>
+
+                    <div
+                        className="hover:bg-[#4da452] cursor-pointer text-white flex justify-center items-center w-40 py-2"
+                        onClick={() => {
+                            navigate('/aboutUs')
+                        }}
+                    >
+                        <i className="mdi mdi-developer-board px-2"></i>
+                        เกี่ยวกับเรา
+                    </div>
+                    {/* <div
+                        className="hover:bg-[#4da452] cursor-pointer text-white flex justify-center items-center w-40 py-4"
+                        onClick={() => {
+                            navigate('/profile')
+                        }}
+                    >
+                        <Avatar className="mx-2 w-8 h-8" {...config} />
+                        {profile.username}
+                    </div> */}
+                    {/* <div className="hover:bg-[#4da452] cursor-pointer text-white flex justify-center items-center w-20 py-4"> */}
+                    <NotificationIcon />
+                    <ProfileIcon />
+                    {/* </div> */}
+                    {/* <div
+                        className="hover:bg-[#4da452] cursor-pointer text-white flex justify-center items-center w-40 py-2"
+                        onClick={() => setOpen(true)}
+                    >
+                        <i className="mdi mdi-logout p-1"></i>
+                        ออกจากระบบ
+                    </div> */}
                 </div>
             </div>
             <main>{children}</main>
