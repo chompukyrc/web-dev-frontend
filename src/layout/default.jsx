8f4011224bb59a3a1516098ff4f100ef02805cbc
mmayyiisuay <queenzio@MacMay.local>
2023-04-27 22:30:24 +0700

    responsive nav

--- src/layout/default.jsx
+++ src/layout/default.jsx
@@ -42,42 +42,6 @@ const Layout = ({ children }) => {
 
     return (
         <div>
-            {/* <nav class="flex items-center justify-between flex-wrap p-6">
-                <div class="block lg:hidden">
-                    <button class="flex items-center px-3 py-2 border rounded text-gray-300 border-gray-400 hover:text-white hover:border-white">
-                        <svg
-                            class="fill-current h-3 w-3"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <title>Menu</title>
-                            <path d="M0 3a3 3 0 013-3h14a3 3 0 110 6H3a3 3 0 01-3-3zm0 8a3 3 0 013-3h14a3 3 0 110 6H3a3 3 0 01-3-3zm0 8a3 3 0 013-3h14a3 3 0 110 6H3a3 3 0 01-3-3z" />
-                        </svg>
-                    </button>
-                </div>
-                <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-                    <div class="text-sm lg:flex-grow">
-                        <a
-                            href="#"
-                            class="block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white mr-4"
-                        >
-                            Home
-                        </a>
-                        <a
-                            href="#"
-                            class="block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white mr-4"
-                        >
-                            About
-                        </a>
-                        <a
-                            href="#"
-                            class="block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white"
-                        >
-                            Contact
-                        </a>
-                    </div>
-                </div>
-            </nav> */}
             <div className="bg-[#60B664] h-[60px] md:h-auto flex w-[100%] justify-between items-center font-Kanit text-[20px]">
                 <div
                     className="flex flex-row items-center text-white cursor-pointer md:ml-10% ml-[5%]"
